f66d987a5b797d7c6a31efe805d9a852a1af7aba
ryan-m-walker <ryan.matthew.walker@gmail.com>
2024-11-23 01:24:27 -0700

    add hover border for diagnostics

--- lua/custom/lsp.lua
+++ lua/custom/lsp.lua
@@ -1,2 +1,10 @@
 vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' })
+
+-- Configure diagnostic floating window to have a rounded border
+vim.diagnostic.config {
+  float = {
+    border = 'rounded',
+    style = 'minimal',
+  },
+}
 vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'rounded' })

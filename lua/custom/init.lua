3ab981726ac4e081e784daef4b1f2055978ff340
ryan-m-walker <ryan.matthew.walker@gmail.com>
2024-11-27 11:03:26 -0700

    add function for moving values from register to default register

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -20,3 +20,11 @@ require('colorizer').setup()
 -- Buffer deletion keymaps
 vim.keymap.set('n', '<leader>x', ':bd<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>X', ':bufdo bd<CR>', { noremap = true, silent = true })
+
+-- Function to move register content to default register
+vim.keymap.set('n', '<leader>m', function()
+  -- Get next character input
+  local char = vim.fn.nr2char(vim.fn.getchar())
+  -- Move content from specified register to default register
+  vim.fn.setreg('"', vim.fn.getreg(char))
+end, { desc = "[M]ove register [Name]'s content to default register" })

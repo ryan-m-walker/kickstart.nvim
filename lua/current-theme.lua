439bd3152f7884313ec137f9bdf9ed7702c7408c
ryan-m-walker <ryan.matthew.walker@gmail.com>
2024-12-08 13:45:01 -0700

    add theme toggle command

--- lua/current-theme.lua
+++ lua/current-theme.lua
@@ -10,9 +10,29 @@ require('nightfox').setup {
   },
 }
 
+-- Initialize current theme state
+_G.current_theme = 'monokai-pro'
+
+-- Function to toggle between themes
+local function toggle_theme()
+  if _G.current_theme == 'monokai-pro' then
+    vim.cmd 'colorscheme dawnfox'
+    _G.current_theme = 'dawnfox'
+  else
+    vim.cmd 'colorscheme monokai-pro'
+    _G.current_theme = 'monokai-pro'
+  end
+end
+
+-- Set initial theme based on system preference
 local theme = vim.fn.system 'defaults read -g AppleInterfaceStyle'
 if theme:match 'Dark' then
   vim.cmd 'colorscheme monokai-pro'
+  _G.current_theme = 'monokai-pro'
 else
   vim.cmd 'colorscheme dawnfox'
+  _G.current_theme = 'dawnfox'
 end
+
+-- Add the toggle theme keymap
+vim.keymap.set('n', '<leader>tt', toggle_theme, { noremap = true, silent = true, desc = '[T]oggle [T]heme' })
